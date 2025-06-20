a1969b05df6c6d1811020af0c7dc6437cc583408
jacklayfield <layf100@gmail.com>
2025-06-20 09:58:25 -0400

    Added isolation forest method for anomoly detection

--- machine_learning/anomaly_detection.py
+++ machine_learning/anomaly_detection.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import psycopg2
+import numpy as np
+from sklearn.ensemble import IsolationForest
 from scipy.stats import zscore
 
 def get_weather_data():
@@ -17,6 +19,7 @@ def get_weather_data():
 
     return df
 
+# Detect anomalies
 def detect_anomalies(df, z_thresh=2.5):
     df = df.sort_values(by=["city", "date"]).copy()
 
@@ -31,7 +34,31 @@ def detect_anomalies(df, z_thresh=2.5):
 
     return pd.concat(anomalies)
 
+# Run Isolation Forest
+def detect_anomalies_isolation_forest(df):
+    df = df.copy()
+    df['temperature_max'] = df['temperature_max'].ffill()
+
+    all_anomalies = []
+
+    for city in df['city'].unique():
+        city_df = df[df['city'] == city].sort_values('date').copy()
+
+        city_df['dayofyear'] = city_df['date'].dt.dayofyear
+
+        X = city_df[['temperature_max', 'dayofyear']]
+
+        iso = IsolationForest(contamination=0.05, random_state=42)
+        city_df['anomaly'] = iso.fit_predict(X)
+        city_df['anomaly'] = city_df['anomaly'].map({1: 0, -1: 1})
+
+        all_anomalies.append(city_df)
+
+    return pd.concat(all_anomalies)
+
 if __name__ == "__main__":
     df = get_weather_data()
     anomaly_df = detect_anomalies(df)
     print(anomaly_df.head(100))
+    anomaly_df_if = detect_anomalies(df)
+    print(anomaly_df_if.head(100))
